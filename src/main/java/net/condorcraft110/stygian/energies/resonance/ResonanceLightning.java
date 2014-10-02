439b200714dce9c5b5366a04da2e5ade7406e196
condorcraft110 <project.ice3@gmail.com>
2014-10-02 21:01:39 +0100

    Switched lightning resonance to vanilla lightning
    ...until I can make dark lightning work properly

--- src/main/java/net/condorcraft110/stygian/energies/resonance/ResonanceLightning.java
+++ src/main/java/net/condorcraft110/stygian/energies/resonance/ResonanceLightning.java
@@ -3,6 +3,7 @@ package net.condorcraft110.stygian.energies.resonance;
 import net.minecraft.item.*;
 import net.minecraft.world.*;
 import net.minecraft.entity.*;
+import net.minecraft.entity.effect.*;
 import net.minecraft.entity.player.*;
 import net.condorcraft110.stygian.entity.*;
 import net.condorcraft110.stygian.energies.resonance.*;
@@ -16,7 +17,9 @@ public class ResonanceLightning implements IResonance
 	
 	public boolean hitEntity(ItemStack stack, EntityLivingBase victim, EntityLivingBase attacker)
 	{
-		EntityDarkLightning lightning = new EntityDarkLightning(victim.worldObj, victim.posX, victim.posY, victim.posZ);
+		//EntityDarkLightning lightning = new EntityDarkLightning(victim.worldObj, victim.posX, victim.posY, victim.posZ);
+		
+		EntityLightningBolt lightning = new EntityLightningBolt(victim.worldObj, victim.posX, victim.posY, victim.posZ);
 		
 		victim.worldObj.spawnEntityInWorld(lightning);
 		
@@ -25,7 +28,9 @@ public class ResonanceLightning implements IResonance
 	
 	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int i, float f, float g, float h)
 	{
-		EntityDarkLightning lightning = new EntityDarkLightning(world, x, y, z);
+		//EntityDarkLightning lightning = new EntityDarkLightning(world, x, y, z);
+		
+		EntityLightningBolt lightning = new EntityLightningBolt(world, x, y, z);
 		
 		world.spawnEntityInWorld(lightning);
 		
